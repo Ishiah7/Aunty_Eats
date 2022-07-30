34635ef7c3a3efbed818fa3318ccef51d52b83c5
Shivern <shivern644@gmail.com>
2022-07-30 12:09:26 +0200

    Menu Item Detailed Screen with Checkout Basket.

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import RestaurantDetailsPage from "./src/screens/RestaurantDetailsScreen";
+import DishDetailsScreen from "./src/screens/DishDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RestaurantDetailsPage />
+      <DishDetailsScreen />
 
       <StatusBar style="auto" />
     </View>
