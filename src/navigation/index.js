bb34e64fb1f77876fc445bece9cf5adc8b648209
Shivern <shivern644@gmail.com>
2022-08-07 13:09:33 +0200

    Added navigation on multiple screens

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,23 +1,80 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import RestaurantDetailsScreen from "../screens/RestaurantDetailsScreen";
 import DishDetailsScreen from "../screens/DishDetailsScreen";
 import Basket from "../screens/Basket";
 import OrderScreen from "../screens/OrdersScreen";
 import OrderDetails from "../screens/OrderDetails";
+import { Foundation, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Restaurant">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen
-        name="Restaurant"
-        component={RestaurantDetailsScreen}
-        options={{ headerShown: false }}
-      />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="HomeTabs" component={HomeTabs} />
     </Stack.Navigator>
+  );
+};
+
+const Tab = createMaterialBottomTabNavigator();
+
+const HomeTabs = () => {
+  return (
+    <Tab.Navigator barStyle={{ backgroundColor: "white" }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeStackNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Foundation name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Orders"
+        component={OrderStackNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="list-alt" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user-alt" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const HomeStack = createNativeStackNavigator();
+
+const HomeStackNavigator = () => {
+  return (
+    <HomeStack.Navigator screenOptions={{ headerShown: false }}>
+      <HomeStack.Screen name="Restaurants" component={HomeScreen} />
+      <HomeStack.Screen name="Restaurant" component={RestaurantDetailsScreen} />
+      <HomeStack.Screen name="Dish" component={DishDetailsScreen} />
+      <HomeStack.Screen name="Basket" component={Basket} />
+    </HomeStack.Navigator>
+  );
+};
+
+const OrderStacks = createNativeStackNavigator();
+
+const OrderStackNavigator = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Orders" component={OrderScreen} />
+      <HomeStack.Screen name="Order" component={OrderDetails} />
+    </HomeStack.Navigator>
   );
 };
 
