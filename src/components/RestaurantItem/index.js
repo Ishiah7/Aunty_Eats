e5c19f1fd0d7bac5f11b1c47e840eb53a9920887
Shivern <shivern644@gmail.com>
2022-08-07 12:16:50 +0200

    Implemented goBack functionality to navigator

--- src/components/RestaurantItem/index.js
+++ src/components/RestaurantItem/index.js
@@ -1,29 +1,38 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const RestaurantItem = ({ restaurant }) => {
+  const Navigation = useNavigation();
+
+  const onPress = () => {
+    Navigation.navigate("Restaurant", { id: restaurant.id });
+  };
+
   return (
-    <View style={styles.restaurantContainer}>
-      <Image
-        source={{ uri: restaurant.image }}
-        style={{
-          width: "100%",
-          aspectRatio: 5 / 3,
-          borderRadius: 5,
-        }}
-      />
-      <View style={styles.row}>
-        <View>
-          <Text style={styles.name}>{restaurant.title}</Text>
-          <Text style={styles.subtitle}>
-            R {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
-            {restaurant.maxDeliveryTime} Minutes
-          </Text>
-        </View>
-        <View style={styles.rating}>
-          <Text style={styles.ratingFont}>{restaurant.rating}</Text>
+    <Pressable onPress={onPress} style={styles.restaurantContainer}>
+      <View style={styles.restaurantContainer}>
+        <Image
+          source={{ uri: restaurant.image }}
+          style={{
+            width: "100%",
+            aspectRatio: 5 / 3,
+            borderRadius: 5,
+          }}
+        />
+        <View style={styles.row}>
+          <View>
+            <Text style={styles.name}>{restaurant.title}</Text>
+            <Text style={styles.subtitle}>
+              R {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
+              {restaurant.maxDeliveryTime} Minutes
+            </Text>
+          </View>
+          <View style={styles.rating}>
+            <Text style={styles.ratingFont}>{restaurant.rating}</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </Pressable>
   );
 };
 
--- src/screens/RestaurantDetailsScreen/index.js
+++ src/screens/RestaurantDetailsScreen/index.js
@@ -1,22 +1,35 @@
-import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+import { View, FlatList } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import DishListItem from "../../components/DishListItem";
 import restaurants from "../../../assets/data/restaurants.json";
 import Header from "../../screens/RestaurantDetailsScreen/Header";
 import styles from "./styles";
+import { useRoute, useNavigation } from "@react-navigation/native";
 
 const restaurant = restaurants[0];
 
 const RestaurantDetailsPage = () => {
+  const route = useRoute();
+  const navigation = useNavigation();
+
+  const id = route.params?.id;
+  console.warn(id);
+
   return (
     <View style={styles.page}>
       <FlatList
         ListHeaderComponent={() => <Header restaurant={restaurant} />}
         data={restaurant.dishes}
         renderItem={({ item }) => <DishListItem dish={item} />}
+        keyExtractor={(item) => item.name}
       />
       <View style={styles.iconContainer}>
-        <Ionicons name="arrow-back-circle" size={45} color="white" />
+        <Ionicons
+          onPress={() => navigation.goBack()}
+          name="arrow-back-circle"
+          size={45}
+          color="white"
+        />
       </View>
     </View>
   );
